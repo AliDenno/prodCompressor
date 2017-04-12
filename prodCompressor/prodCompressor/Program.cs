588f81e7a25218f206e852b317cf3c00bcd2cdd4
Ali Denno <aliedenno@gmail.com>
2017-04-12 20:19:20 +0200

    Add: minifier

--- prodCompressor/prodCompressor/Program.cs
+++ prodCompressor/prodCompressor/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace prodCompressor
 {
@@ -10,6 +14,11 @@ namespace prodCompressor
     {
         static void Main(string[] args)
         {
+            var sampleXml = File.ReadAllText(@"Resources\1.xmf");
+            var minifiedXml = new XMLMinifier(XMLMinifierSettings.Aggressive).Minify(sampleXml);
+            var path = @"Resources\Result\1.xmf";
+            File.WriteAllText(path, minifiedXml);
+            Console.WriteLine("Done");
         }
     }
 }
